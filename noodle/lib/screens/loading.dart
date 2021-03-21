ab7fe248929d3fd2745a8e53c15187c41d9a2165
Khai Truong <kd.kidr@gmail.com>
2021-03-21 22:21:00 +0700

    Make loading screen stateful

--- noodle/lib/screens/loading.dart
+++ noodle/lib/screens/loading.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Loading extends StatelessWidget {
-  const Loading({Key key}) : super(key: key);
+class Loading extends StatefulWidget {
+  Loading({Key key}) : super(key: key);
 
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
