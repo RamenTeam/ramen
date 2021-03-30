34e1c7a9f8699a547aa1eaa7fca8c7cf27325075
Chung Quan Tin <s3818487@rmit.edu.vn>
2021-03-30 12:26:32 +0700

    :heavy_plus_sign: Add a events service in webrtc server
    By using nestjs and socketio to implement a webrtc server

---
+++ kansui/src/events/events.gateway.ts
@@ -0,0 +1,34 @@
+import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import { EventsService } from './events.service';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+
+@WebSocketGateway()
+export class EventsGateway {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @SubscribeMessage('createEvent')
+  create(@MessageBody() createEventDto: CreateEventDto) {
+    return this.eventsService.create(createEventDto);
+  }
+
+  @SubscribeMessage('findAllEvents')
+  findAll() {
+    return this.eventsService.findAll();
+  }
+
+  @SubscribeMessage('findOneEvent')
+  findOne(@MessageBody() id: number) {
+    return this.eventsService.findOne(id);
+  }
+
+  @SubscribeMessage('updateEvent')
+  update(@MessageBody() updateEventDto: UpdateEventDto) {
+    return this.eventsService.update(updateEventDto.id, updateEventDto);
+  }
+
+  @SubscribeMessage('removeEvent')
+  remove(@MessageBody() id: number) {
+    return this.eventsService.remove(id);
+  }
+}
---
+++ kansui/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT);
+}
+bootstrap();
