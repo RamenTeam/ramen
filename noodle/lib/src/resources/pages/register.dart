cc91622c2b0d3357daa0cc48701e76aa29cb9bde
kasturez <tinhuynhvinh733@gmail.com>
2021-03-30 21:39:20 +0700

    Update register.dart

--- noodle/lib/src/resources/pages/register.dart
+++ noodle/lib/src/resources/pages/register.dart
@@ -8,10 +8,44 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final userNameField = TextField(
+    decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32)),
+        hintText: 'Enter username'),
+  );
+  final emailField = TextField(
+    decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32)),
+        hintText: 'Enter email'),
+  );
+  final passwordField = TextField(
+    decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32)),
+        hintText: 'Enter password'),
+  );
+  final phoneField = TextField(
+    decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32)),
+        hintText: 'Enter phone number'),
+  );
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Register'),
-    );
+    return Scaffold(
+        body: Center(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/images/logo.png'),
+          Text('Welcome to Ramen'),
+          userNameField,
+          emailField,
+          passwordField,
+          phoneField
+        ],
+      ),
+    ));
   }
 }
