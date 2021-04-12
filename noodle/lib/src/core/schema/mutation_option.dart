f02dc0c0e1090f55851742fbad15a6f4acbb0415
Khai Truong <kd.kidr@gmail.com>
2021-04-12 21:02:09 +0700

    Change graphql library

--- noodle/lib/src/core/schema/mutation_option.dart
+++ noodle/lib/src/core/schema/mutation_option.dart
@@ -1,6 +1,6 @@
 // ignore: import_of_legacy_library_into_null_safe
-import 'package:graphql/client.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-MutationOptions getMutationOptions({required String schema, dynamic variables}) {
+MutationOptions getMutationOptions({required String schema, Map<String,dynamic>? variables}) {
   return MutationOptions(document: gql(schema), variables: variables);
 }
