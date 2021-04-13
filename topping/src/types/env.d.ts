20f6e4da52bc976c7cb19ca1b747c326609642ff
Chung Quan Tin <s3818487@rmit.edu.vn>
2021-04-13 11:38:13 +0700

    add docker compose

--- topping/src/types/env.d.ts
+++ topping/src/types/env.d.ts
@@ -1,15 +1,14 @@
 declare namespace NodeJS {
-  interface ProcessEnv {
-    SESSION_SECRET: string;
-    CLIENT_PORT: string;
-    CLIENT_PORT: string;
-    POSTGRES_URL: string;
-    POSTGRES_USER: string;
-    POSTGRES_PASSWORD: string;
-    MONGODB_URI: string;
-    SERVER_URI: string;
-    SERVER_ENDPOINT: string;
-    TEST_HOST: string;
-    PROD_SERVER_HOST: string;
-  }
+	interface ProcessEnv {
+		SESSION_SECRET: string;
+		CLIENT_PORT: string;
+		POSTGRES_URL: string;
+		POSTGRES_USER: string;
+		POSTGRES_PASSWORD: string;
+		MONGODB_URI: string;
+		SERVER_URI: string;
+		SERVER_ENDPOINT: string;
+		TEST_HOST: string;
+		PROD_SERVER_HOST: string;
+	}
 }
