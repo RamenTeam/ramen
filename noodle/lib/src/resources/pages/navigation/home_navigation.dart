8caded6cc1fa02aeed832ccd25bcbeae7612cbff
Khai Truong <kd.kidr@gmail.com>
2021-04-14 01:39:54 +0700

    Resolve conflict

--- noodle/lib/src/resources/pages/navigation/home_navigation.dart
+++ noodle/lib/src/resources/pages/navigation/home_navigation.dart
@@ -9,50 +9,24 @@ import 'package:noodle/src/resources/pages/profile/profile.dart';
 import 'package:noodle/src/resources/pages/interaction/meeting.dart';
 import 'package:noodle/src/resources/theme/theme.dart';
 
-class HomeNavigation extends StatefulWidget {
-  HomeNavigation({Key? key}) : super(key: key);
-
-  static Route route() {
-    return MaterialPageRoute<void>(builder: (_) => HomeNavigation());
-  }
-
-  @override
-  _HomeNavigationState createState() => _HomeNavigationState();
-}
-
-class _HomeNavigationState extends State<HomeNavigation> {
-  int tabIndex = 0;
-  final List<Widget> tabs = [
-    HomeScreen(),
-    ProfileScreen(),
-    MeetingScreen(),
-  ];
-
-  AppTheme? _theme;
-
-  @override
-  void didChangeDependencies() {
-    if (_theme == null) {
-      _theme = AppTheme.of(context);
-
-// class HomeNavigation extends StatelessWidget {
-//   Widget currentTab(int tabIndex) {
-//     switch (tabIndex) {
-//       case 0:
-//         return HomeScreen();
-//       case 1:
-//         return ProfileScreen();
-//       default:
-//         return Container();
+class HomeNavigation extends StatelessWidget {
+  Widget currentTab(int tabIndex) {
+    switch (tabIndex) {
+      case 0:
+        return HomeScreen();
+      case 1:
+        return ProfileScreen();
+      default:
+        return Container();
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider<TabNavigationBloc>(
-        create: (_) => TabNavigationBloc(initialTabIndex: 0),
-        child:
-            BlocBuilder<TabNavigationBloc, int>(builder: (builder, tabIndex) {
+      create: (_) => TabNavigationBloc(initialTabIndex: 0),
+      child: BlocBuilder<TabNavigationBloc, int>(
+        builder: (builder, tabIndex) {
           return SafeArea(
             child: Scaffold(
               body: currentTab(tabIndex),
@@ -61,13 +35,8 @@ class _HomeNavigationState extends State<HomeNavigation> {
                 tabIndex: tabIndex,
               ),
             ),
-            BottomNavigationBarItem(
-              icon: FaIcon(FontAwesomeIcons.userAstronaut),
-              label: 'Meetin',
-            ),
-          ],
-          onTap: onTabSelected,
-        ),
+          );
+        },
       ),
     );
   }
