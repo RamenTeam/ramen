8f47e4b33c0657ccda2272482c1d1c547276f6e5
Khai Truong <kd.kidr@gmail.com>
2021-04-14 20:06:16 +0700

    Profile fetch current user

--- noodle/lib/src/core/bloc/profile/profile_bloc.dart
+++ noodle/lib/src/core/bloc/profile/profile_bloc.dart
@@ -1,13 +1,23 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:noodle/src/core/bloc/profile/profile_event.dart';
 import 'package:noodle/src/core/bloc/profile/profile_state.dart';
+import 'package:noodle/src/core/models/user.dart';
+import 'package:noodle/src/core/repositories/user_repository.dart';
 
 class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
-  ProfileBloc({required user}) : super(ProfileState(user: user));
+  ProfileBloc({required this.userRepository}) : super(ProfileLoadingState()) {
+    add(FetchUser());
+  }
+
+  final UserRepository userRepository;
 
   @override
   Stream<ProfileState> mapEventToState(ProfileEvent event) async* {
-    if (event is ChangeAvatarRequest) {
+    if (event is FetchUser) {
+      User? user = await userRepository.getUser();
+      print(user.toString());
+      if (user != null) yield ProfileState(user: user);
+    } else if (event is ChangeAvatarRequest) {
       // show actions
     } else if (event is OpenImagePicker) {
       // open image picker
--- noodle/lib/src/resources/pages/navigation/home_navigation.dart
+++ noodle/lib/src/resources/pages/navigation/home_navigation.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:noodle/src/core/bloc/profile/profile_bloc.dart';
 import 'package:noodle/src/core/bloc/tab_navigation/tab_navigation_bloc.dart';
 import 'package:noodle/src/core/bloc/tab_navigation/tab_navigation_event.dart';
+import 'package:noodle/src/core/repositories/user_repository.dart';
 import 'package:noodle/src/resources/pages/home/home.dart';
 import 'package:noodle/src/resources/pages/navigation/local_widget/home_bottom_nav_bar.dart';
 import 'package:noodle/src/resources/pages/profile/profile.dart';
 import 'package:noodle/src/resources/pages/interaction/meeting.dart';
 import 'package:noodle/src/resources/theme/theme.dart';
+import 'package:provider/provider.dart';
 
 class HomeNavigation extends StatelessWidget {
   Widget currentTab(int tabIndex) {
@@ -25,21 +28,30 @@ class HomeNavigation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<TabNavigationBloc>(
-      create: (_) => TabNavigationBloc(initialTabIndex: 0),
-      child: BlocBuilder<TabNavigationBloc, int>(
-        builder: (builder, tabIndex) {
-          return SafeArea(
-            child: Scaffold(
-              body: currentTab(tabIndex),
-              backgroundColor: Theme.of(context).accentColor,
-              bottomNavigationBar: HomeBottomNavigationBar(
-                tabIndex: tabIndex,
-              ),
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider<TabNavigationBloc>(
+            create: (_) => TabNavigationBloc(initialTabIndex: 0),
+          ),
+          BlocProvider<ProfileBloc>(
+            create: (_) => ProfileBloc(
+              userRepository:
+                  Provider.of<UserRepository>(context, listen: false),
             ),
-          );
-        },
-      ),
-    );
+          ),
+        ],
+        child: BlocBuilder<TabNavigationBloc, int>(
+          builder: (builder, tabIndex) {
+            return SafeArea(
+              child: Scaffold(
+                body: currentTab(tabIndex),
+                backgroundColor: Theme.of(context).accentColor,
+                bottomNavigationBar: HomeBottomNavigationBar(
+                  tabIndex: tabIndex,
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
