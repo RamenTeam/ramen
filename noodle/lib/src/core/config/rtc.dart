a1208d8c1df06dde87048df9483767ebbfbd2b28
Khai Truong <kd.kidr@gmail.com>
2021-05-05 23:39:11 +0700

    Finish deploy server + Add remote rtc host to Flutter app

--- noodle/lib/src/core/config/rtc.dart
+++ noodle/lib/src/core/config/rtc.dart
@@ -1,7 +1,8 @@
 import 'package:noodle/src/core/config/rtc_p2p.dart';
 import 'package:noodle/src/core/config/rtc_signaling.dart';
 
-final RTCSignaling rtcSignaling =
-    new RTCSignaling(host: "127.0.0.1", port: 3000);
+const String LOCAL = "127.0.0.1";
+const String REMOTE = "https://ramen-kansui.herokuapp.com/";
+final RTCSignaling rtcSignaling = new RTCSignaling(host: REMOTE, port: 3000);
 
 final RTCPeerToPeer rtcPeerToPeer = new RTCPeerToPeer();
